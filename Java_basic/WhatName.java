bfbe942cff30b47f2c715a0bb691b788b4db4708
unknown <boyseos@gmail.com>
2019-08-09 17:44:14 +0900

    한글테스트

--- Java_basic/WhatName.java
+++ Java_basic/WhatName.java
@@ -1,5 +1,8 @@
 class WhatName{
     public static void main(String[] args){
-        System.out.print("What Name");
+        System.out.print("이름이 뭐니");
+        String name = "xxx"; //변수선언
+        int age = 0;
+        System.out.print("나는 " + name + "야. 내 나이는 "+ age);
     }
 }
