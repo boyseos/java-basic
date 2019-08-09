4ca1ba8c891c00410b5d2b3c43ffcf34fd76d0f7
unknown <boyseos@gmail.com>
2019-08-09 18:10:03 +0900

    완성형

--- Java_basic/WhatName.java
+++ Java_basic/WhatName.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 class WhatName{
     public static void main(String[] args){
-        System.out.print("이름이 뭐니");
+        System.out.println("이름이 뭐니");
         String name = "xxx"; //변수선언
+
+        //값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.print("나는 " + name +"라고 해...!!!\n");
+        System.out.println("어르신 연세가...");
         int age = 0;
-        System.out.print("나는 " + name + "야. 내 나이는 "+ age);
+        age = scanner.nextInt();
+        System.out.print("나이는 "+ age + "라구..");
     }
 }
